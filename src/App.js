8850e5e74d6f1dae537fb9d6d71f96c7a8bd2061
Del <delia.chassaing@gmail.com>
2017-08-29 13:13:38 -0500

    React start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import react-scripts from 'react-scripts';
 
-class App extends Component {
+class FormStructure extends React.Component {
+  renderButton(i) {
+    return <Button value={i}/> (
+
+    <div className="form-structure">
+    <h1>Score for {this.props.name}</h1>
+    <ul>
+      <li><form>My current handicap</form></li>
+      <li><form>My Score</form></li>
+      <li><form>Course Rating</form></li>
+      <li><form>Course Slope</form></li>
+    </ul>
+    </div>
+    );
+  }
+}
+
+class Button extends React.Component {
   render() {
     return (
-    <div>
-      <button onClick = {this.handleClick}>Calculate my Score</button>
-    </div>
+      <div>
+        <button className ="submit"> onCLick={() => alert('click')}</button>
+        {this.props.value}
+        // onClick = {this.handleClick}>Calculate my Score
+      </div>
+    );
+  }
+}
+
+class FormInput extends React.Component {
+  render() {
+    return (
+      <div className = "form-input">
+
+      </div>
     );
   }
 }
